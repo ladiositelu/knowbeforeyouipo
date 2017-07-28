00cb1aabfa4c111deb6a97a6c713d63b58bd1aa3
Ladi Ositelu <oladipoositelu@gmail.com>
2017-07-28 13:38:14 -0400

    Update Scrape_S1_documents.py

--- Scrape_S1_documents.py
+++ Scrape_S1_documents.py
@@ -2,19 +2,13 @@
 import pandas as pd
 import datetime
 import sqlite3
-import csv
-import datetime
-import requests
 from sqlalchemy import create_engine
 
-
-
-start_year = 2001
+start_year = 2004
 years = (range(start_year, 2017))
 quarters = ['QTR1', 'QTR2', 'QTR3', 'QTR4']
 
 history = [(y, q) for y in years for q in quarters] # a list of (year, quarter) tuples
-
 
 # Create a list of the url names for the years and quarters of interest
 urls = ['https://www.sec.gov/Archives/edgar/full-index/%d/%s/master.idx' % (x[0], x[1]) for x in history]
@@ -22,8 +16,10 @@ urls.sort()
 
 
 #Creating and connecting to sqlite database file
-con = sqlite3.connect('/.../edgar_idx.db')
+con = sqlite3.connect('/Users/oladipoositelu/Desktop/KBYIPO/databases/edgar_idx.db')
 cur = con.cursor()
+
+print "connected to sqlite database"
 
 #Create a new table with the following columns:
 #1. cik==, 2. conm == company name, 3. type == type of form,
@@ -42,7 +38,6 @@ for url in urls:
 con.commit()
 con.close()
 
-engine = create_engine('sqlite:///edgar_idx.db')
-with engine.connect() as conn, conn.begin():
-    data = pd.read_sql_table('idx', conn)
-    data[data['type']=='S-1'].to_csv('/.../Form_S1.csv',index=False) ##Save only the form S-1s
+print "connection closed"
+
+
